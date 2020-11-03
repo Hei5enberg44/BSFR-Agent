4851424a44dbf5ecc4775ae8322983ee7d61755d
Kylian BARUSSEAU <kylian.barusseau@omedan.me>
2020-11-03 23:30:06 +0100

    WIP

--- bot/commands/RraddCommand.js
+++ bot/commands/RraddCommand.js
@@ -9,6 +9,33 @@ class HelpCommand {
         this.commands = opt.commands;
         this.utils = opt.utils;
         this.config = opt.config;
+
+        this.emojis = {
+            0: "🇦",
+            1: "🇧",
+            2: "🇨",
+            3: "🇩",
+            4: "🇪",
+            5: "🇫",
+            6: "🇬",
+            7: "🇭",
+            8: "🇮",
+            9: "🇯",
+            10: "🇰",
+            11: "🇱",
+            12: "🇲",
+            13: "🇳",
+            14: "🇴",
+            15: "🇵",
+            16: "🇶",
+            17: "🇷",
+            18: "🇸",
+            19: "🇹"
+            // 20: "🇧",
+            // 21: "🇧",
+            // 22: "🇧",
+            // 23: "🇧",
+        }
     }
 
     /**
@@ -32,7 +59,33 @@ class HelpCommand {
      * @param message
      */
     async exec(args, message) {
-        console.log(args, message)
+        if(args.length < 3) {
+            await message.channel.send("> :x: Merci d'indiquer un channel, un message ainsi qu'au moins 1 rôle")
+        }
+
+        let guild = this.clients.discord.getClient().guilds.cache.get(this.config.ids.guild)
+
+        let channel = await guild.channels.cache.get(args[0]);
+
+        // if(!channel) {
+        //     await message.channel.send("> :x: Le channel ne semble pas existé")
+        // }
+
+        let roleNumbers = args.length - 2;
+        let msg = args[1].replace(/`/g, '') + "\n";
+
+        for(let i = 0; i < roleNumbers; i++) {
+            msg += "\n";
+
+            let role = await guild.roles.cache.get(args[i + 2])
+
+            msg += this.emojis[i] + ": " + role.name
+        }
+
+        channel.send(msg)
+
+
+        // console.log(args, message)
     }
 
 }
