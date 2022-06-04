bad9261384d666e248467623543fcbba34413629
Hei5enberg44 <antoineboulet44@gmail.com>
2022-06-04 04:02:06 +0200

    Mise en place d'un système de pagination pour la sélection de la ville
    Mise en place d'un système de pagination pour palier au fait que Discord n'autorise seulement 5 lignes de 5 boutons maximum

--- commands/city.js
+++ commands/city.js
@@ -45,38 +45,82 @@ module.exports = {
                     const cities = await city.getCitiesByPostalCode(postalCode)
 
                     if(cities.length > 0) {
-                        let components = []
-                        for(let row = 0; row < Math.ceil(cities.length / 5); row++) {
-                            const citiesButtons = new MessageActionRow()
-                            const citiesRow = cities.slice(row * 5, row * 5 + 5)
-                            for(const c of citiesRow) {
-                                citiesButtons.addComponents(
-                                    new MessageButton()
-                                        .setCustomId(`${c.code_postal},${c.nom_de_la_commune}`)
-                                        .setLabel(c.nom_de_la_commune)
-                                        .setStyle('PRIMARY')
-                                )
+                        const maxRows = 4
+                        const maxItemsPerRow = 5
+                        const pageCount = Math.ceil(cities.length / (maxRows * maxItemsPerRow))
+                        let page = 0
+
+                        const getComponents = function(page = 0) {
+                            let components = []
+
+                            const _cities = cities.slice(page * maxRows * maxItemsPerRow, (page * maxRows * maxItemsPerRow) + (maxRows * maxItemsPerRow))
+                            for(let row = 0; row < Math.ceil(_cities.length / 5); row++) {
+                                const citiesButtons = new MessageActionRow()
+                                const citiesRow = _cities.slice(row * maxItemsPerRow, (row * maxItemsPerRow) + maxItemsPerRow)
+    
+                                for(const c of citiesRow) {
+                                    citiesButtons.addComponents(
+                                        new MessageButton()
+                                            .setCustomId(`${c.code_postal},${c.nom_de_la_commune}`)
+                                            .setLabel(c.nom_de_la_commune)
+                                            .setStyle('SECONDARY')
+                                    )
+                                }
+                                components.push(citiesButtons)
                             }
+
+                            const citiesButtons = new MessageActionRow()
+                            citiesButtons.addComponents(
+                                new MessageButton()
+                                    .setCustomId('previous')
+                                    .setLabel('Précédent')
+                                    .setStyle('PRIMARY')
+                                    .setDisabled(page > 0 ? false : true),
+                                new MessageButton()
+                                    .setCustomId('next')
+                                    .setLabel('Suivant')
+                                    .setStyle('PRIMARY')
+                                    .setDisabled(page < pageCount - 1 ? false : true)
+                            )
                             components.push(citiesButtons)
+
+                            return components
                         }
+
+                        let components = getComponents()
 
                         await interaction.reply({ content: 'Veuillez sélectionner votre ville :', components: components, ephemeral: true })
 
                         const filter = i => i.user.id === interaction.user.id
 
-                        const collector = interaction.channel.createMessageComponentCollector({ filter, max: 1, time: 15000 })
+                        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 })
+
+                        collector.on('collect', async i => {
+                            const choice = i.customId
+
+                            if(!['previous', 'next'].find(x => x === choice)) {
+                                await i.deferUpdate()
+
+                                const c = choice.split(',')
+                                await city.set(interaction.user.id, c[0], c[1])
+
+                                await i.editReply({ content: 'Votre ville d\'origine a bien été enregistrée', components: [] })
+
+                                Logger.log('CityCommand', 'INFO', `${interaction.user.tag} a ajouté sa ville d'origine`)
+                            } else {
+                                collector.resetTimer()
+                                await i.deferUpdate()
+
+                                page = choice === 'previous' ? page - 1 : page + 1
+                                components = getComponents(page)
+
+                                await i.editReply({ content: 'Veuillez sélectionner votre ville :', components: components })
+                            }
+                        })
 
                         collector.on('end', async (collected, reason) => {
                             if(reason === 'time') {
                                 await interaction.editReply({ content: 'Vous avez mis trop de temps à répondre', components: [] })
-                            } else {
-                                const c = collected.first().customId.split(',')
-
-                                await city.set(interaction.user.id, c[0], c[1])
-
-                                await interaction.editReply({ content: 'Votre ville d\'origine a bien été enregistrée', components: [] })
-
-                                Logger.log('CityCommand', 'INFO', `${interaction.user.tag} a ajouté sa ville d'origine`)
                             }
                         })
                     } else {
