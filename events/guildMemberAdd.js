cec504f819b60e7754ff67f4f605eb855ef26d18
Hei5enberg44 <antoineboulet44@gmail.com>
2022-08-24 12:24:30 +0200

    Suppression des réactions d'un membre sur le message des règles lorsque celui-ci rejoint le serveur

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,7 @@
 const { GuildMember, userMention } = require('discord.js')
 const Embed = require('../utils/embed')
 const mute = require('../controllers/mute')
+const { Reactions } = require('../controllers/database')
 const Logger = require('../utils/logger')
 const config = require('../config.json')
 
@@ -11,7 +12,8 @@ module.exports = {
 	 */
 	async execute(member) {
 		await module.exports.welcome(member)
-		await mute.remute(member)
+        await module.exports.removeRulesReactions(member)
+        await mute.remute(member)
 	},
 
     /**
@@ -33,5 +35,27 @@ module.exports = {
         await logsChannel.send({ embeds: [embed] })
 
         Logger.log('Join', 'INFO', `Le membre ${member.user.tag} a rejoint le serveur`)
+    },
+
+    /**
+	 * Supprime les réactions d'un membre sur le message des règles du serveur Discord
+	 * @param {GuildMember} member The member that has left/been kicked from the guild
+	 */
+    async removeRulesReactions(member) {
+        const rulesReaction = await Reactions.findOne({ where: { type: 'rules' } })
+
+        if(rulesReaction) {
+            const rulesChannel = member.guild.channels.cache.get(rulesReaction.channelId)
+            if(rulesChannel) {
+                const rulesMessage = await rulesChannel.messages.fetch(rulesReaction.messageId)
+                if(rulesMessage) {
+                    const rulesMessageReactions = rulesMessage.reactions.cache
+                    for(const [, rulesMessageReaction] of rulesMessageReactions) {
+                        await rulesMessageReaction.users.remove(member.user)
+                    }
+                    Logger.log('Rules', 'INFO', `Les réactions sur le message des règles pour le membre ${member.user.tag} ont été supprimées`)
+                }
+            }
+        }
     }
 }
