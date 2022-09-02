f38e930de03bd87bb9de119f1407ca50c2323c2d
Hei5enberg44 <antoineboulet44@gmail.com>
2022-09-02 16:22:22 +0200

    Ajout de paramètres pour la commande /avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, ApplicationCommandOptionType } = require('discord.js')
+const { CommandInteraction, ApplicationCommandOptionType, ALLOWED_EXTENSIONS, ALLOWED_SIZES } = require('discord.js')
 const { CommandError, CommandInteractionError } = require('../utils/error')
 
 module.exports = {
@@ -10,6 +10,30 @@ module.exports = {
                 type: ApplicationCommandOptionType.User,
                 name: 'membre',
                 description: 'Membre pour lequel récupérer l\'avatar',
+                required: false
+            },
+            {
+                type: ApplicationCommandOptionType.String,
+                name: 'extension',
+                description: 'Extension de l\'image',
+                choices: ALLOWED_EXTENSIONS.map(extension => {
+                    return { name: extension, value: extension }
+                }),
+                required: false
+            },
+            {
+                type: ApplicationCommandOptionType.Integer,
+                name: 'taille',
+                description: 'Taille de l\'image',
+                choices: ALLOWED_SIZES.map(size => {
+                    return { name: size, value: size }
+                }),
+                required: false
+            },
+            {
+                type: ApplicationCommandOptionType.Boolean,
+                name: 'statique',
+                description: 'Force l\'affichage statique de l\'image',
                 required: false
             }
         ],
@@ -23,8 +47,11 @@ module.exports = {
     async execute(interaction) {
         try {
             const member = interaction.options.getUser('membre') ?? interaction.user
+            const extension = interaction.options.getString('extension') ?? ALLOWED_EXTENSIONS[1]
+            const size = interaction.options.getInteger('taille') ?? ALLOWED_SIZES[6]
+            const forceStatic = interaction.options.getBoolean('statique') ?? true
 
-            const avatarUrl = member.displayAvatarURL({ dynamic: true, size: 1024, format: 'png' })
+            const avatarUrl = member.displayAvatarURL({ extension: extension, size: size, forceStatic: forceStatic })
 
             await interaction.reply({ content: avatarUrl })
         } catch(error) {
