21bf254b2d4c0c712b90c2037eef5c13d3e301a2
Hei5enberg44 <antoineboulet44@gmail.com>
2023-03-27 12:37:41 +0200

    Optimisations du feur

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,9 @@
 import { Message, userMention } from 'discord.js'
-import crypto from 'crypto'
 import maliciousURL from '../controllers/maliciousURL.js'
 import threads from '../controllers/threads.js'
 import antivirus from '../controllers/antivirus.js'
 import twitch from '../controllers/twitch.js'
-import { Feur } from '../controllers/database.js'
+import feur from '../controllers/feur.js'
 import Logger from '../utils/logger.js'
 import config from '../config.json' assert { type: 'json' }
 
@@ -17,8 +16,8 @@ export default {
         if(message) {
             if(message.guildId) {
                 // Si ce n'est pas un dm
-                await this.feur(message)
-                await this.feurLeaderboard(message)
+                await feur.feur(message)
+                await feur.feurLeaderboard(message)
 
                 // Test si un URL malveillant a été envoyé
                 if(message.content.match(/https?:\/\//)) {
@@ -61,76 +60,6 @@ export default {
     async voteRunReactions(message) {
         await message.react('✅')
         await message.react('❎')
-    },
-
-    /**
-     * Répond "feur" si "quoi" est détécté en fin de phrase
-     * (crédit: Vred#0001)
-     * @param {Message} message The created message
-     */
-    async feur(message) {
-        const random = crypto.randomInt(15)
-        if(message.content.match(/[cqk]+(?:[uúùûü]*w+[uúùûü]*[aáàâäãå]+|[uúùûü]*[eéèêë]*[oóòôöõ]+[iíìîïaáàâäãå]+|[oóòôöõ]+[uúùûü]+[aáàâäãå]+)/igm) && random === 0) {
-            message.reply({ content: 'feur', allowedMentions: { repliedUser: false } })
-            Logger.log('Feur', 'INFO', `Agent a répondu "feur" à ${message.author.tag}`)
-        }
-    },
-
-    /**
-     * Enregistre les réponses "feur" à des messages contenant "quoi" dans la base de données
-     * @param {Message} message The created message
-     */
-    async feurLeaderboard(message) {
-        const regex = new RegExp(/[cqk]+(?:[uúùûü]*w+[uúùûü]*[aáàâäãå]+|[uúùûü]*[eéèêë]*[oóòôöõ]+[iíìîïaáàâäãå]+|[oóòôöõ]+[uúùûü]+[aáàâäãå]+)/, 'igm')
-        const exceptions = [ 'aquoibonisme', 'aquoibonismes', 'aquoiboniste', 'aquoibonistes', 'carquois', 'claquoir', 'claquoirs', 'dacquois', 'dacquoise', 'dacquoises', 'dunkerquois', 'dunkerquoise', 'dunkerquoises', 'iroquoien', 'iroquoienne', 'iroquoiennes', 'iroquoiens', 'iroquois', 'iroquoise', 'iroquoises', 'lucquois', 'lucquoise', 'lucquoises', 'marquoir', 'marquoirs', 'narquois', 'narquoise', 'narquoisement', 'narquoises', 'piquoir', 'piquoirs', 'pourquoi', 'quoi', 'quoique', 'rufisquois', 'rufisquoise', 'rufisquoises', 'sequoia', 'sequoias', 'taquoir', 'taquoirs', 'turquoise', 'turquoises' ]
-
-        const isException = word => {
-            if(word.length > 6 && word.length < 15 && exceptions.find(e => e === word)) 
-                return true
-            return false
-        }
-
-        const messageSplit = message.content.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '').split(' ')
-        if(message.reference && messageSplit.includes('feur')) {
-            const channelReference = message.guild.channels.cache.find(c => c.id === message.reference.channelId)
-            const messageReference = await channelReference.messages.fetch(message.reference.messageId)
-
-            if(messageReference.author.id === message.author.id || messageReference.createdTimestamp + 86400000 < Date.now()) return false
-
-            const sentence = messageReference.content.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '')
-
-            let match
-            while((match = regex.exec(sentence)) !== null) {
-                let startIndex = sentence.substring(0, match.index).lastIndexOf(' ') + 1
-                if(startIndex === 0) startIndex = 0
-
-                let endIndex = regex.lastIndex + sentence.substring(regex.lastIndex).indexOf(' ') + 1
-                if(endIndex === regex.lastIndex) endIndex = sentence.length
-
-                const word = sentence.substring(startIndex, endIndex).trim()
-                if(!isException(word)) {
-                    const feured = await Feur.findOne({
-                        where: {
-                            victimId: messageReference.author.id,
-                            messageId: messageReference.id
-                        }
-                    })
-
-                    if(!feured) {
-                        await Feur.create({
-                            attackerId: message.author.id,
-                            victimId: messageReference.author.id,
-                            messageId: messageReference.id,
-                            message: messageReference.content,
-                            messageDate: new Date(messageReference.createdTimestamp),
-                            responseId: message.id,
-                            response: message.content,
-                            responseDate: new Date(message.createdTimestamp)
-                        })
-                    }
-                }
-            }
-        }
     },
 
     /**
