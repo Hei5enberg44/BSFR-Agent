716fd41832485d75a8e127b7df93d7ff0e8e86f9
Hei5enberg44 <antoineboulet44@gmail.com>
2023-05-01 22:18:44 +0200

    Récupération des informations de mises à jour depuis l'API de Steam

--- controllers/checkBSUpdate.js
+++ controllers/checkBSUpdate.js
@@ -1,16 +1,18 @@
-import { Client, roleMention } from 'discord.js'
-import puppeteer from 'puppeteer'
-import fetch from 'node-fetch'
+import { Client, TextChannel, MessageFlags, roleMention } from 'discord.js'
 import { BSUpdates } from './database.js'
 import Logger from '../utils/logger.js'
 import config from '../config.json' assert { type: 'json' }
 
+const apiUrl = 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=620980&count=1&maxlength=0&format=json'
+const STEAM_CLAN_IMAGE = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans'
+const STEAM_CLAN_LOC_IMAGE = 'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans'
+
 export default {
     /**
      * @typedef {Object} UpdateData
-     * @property {Buffer|null} image
-     * @property {string|null} title
-     * @property {string|null} content
+     * @property {string} image
+     * @property {string} title
+     * @property {Array<string>} content
      */
 
     /**
@@ -18,114 +20,73 @@ export default {
      * @returns {Promise<UpdateData|null>} informations sur la mise à jour
      */
     async getLastUpdate() {
-        const oculusUrl = 'https://www.oculus.com'
-        const beatsaberPath = '/experiences/rift/1304877726278670/'
+        const updateData = {}
 
-        const browser = await puppeteer.launch()
+        const newsRequest = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
-        try {
-            const page = await browser.newPage()
-            await page.goto(oculusUrl + beatsaberPath)
+        if(newsRequest.ok) {
+            const news = await newsRequest.json()
+            const newsItems = news?.appnews?.newsitems
 
-            await page.waitForSelector('a.devpost-tile', {
-                timeout: 10000
-            })
+            if(newsItems) {
+                const lastNews = newsItems[0]
+                
+                updateData.title = `**${lastNews.title}**`
 
-            const lastDevPostPath = await page.evaluate(() => {
-                const devPosts = document.body.querySelectorAll('a.devpost-tile')
-                if(devPosts[0]) return devPosts[0].getAttribute('href')
-                return null
-            })
-
-            let updateInfos = null
-
-            if(lastDevPostPath) {
-                await page.goto(oculusUrl + lastDevPostPath)
-
-                updateInfos = await page.evaluate(() => {
-                    const formatText = (text) => text.replace(/([\*\|_-])/g, '\\$1')
-
-                    const infos = {
-                        image: null,
-                        title: null,
-                        content: null
-                    }
-
-                    const imageContainer = document.body.querySelector('._9cqr')
-                    if(imageContainer) infos.image = imageContainer.src
-
-                    const titleContainer = document.body.querySelector('._9cq4')
-                    if(titleContainer) infos.title = `**${formatText(titleContainer.textContent)}**`
-
-                    const contentContainer = document.body.querySelector('[data-contents="true"]')
-                    if(contentContainer) {
-                        const content = []
-                        for(const child of contentContainer.children) {
-                            if(child.tagName === 'UL') {
-                                const listItems = child.querySelectorAll('li')
-                                for(const li of listItems) {
-                                    let textContent = formatText(li.textContent)
-                                    if(li.style.fontWeight === 'bold') textContent = `**${textContent}**`
-                                    if(li.style.fontStyle === 'italic') textContent = `*${textContent}*`
-                                    textContent = li.classList.contains('_3kq1') ? `• ${textContent}` : `    ○ ${textContent}`
-                                    content.push(textContent)
-                                }
-                            } else if(child.tagName === 'DIV') {
-                                const spans = child.querySelector('div')?.children
-                                if(spans) {
-                                    let textContent = ''
-                                    for(const span of spans) {
-                                        if(span.tagName === 'A') {
-                                            textContent += span.getAttribute('href')
-                                        } else {
-                                            textContent += formatText(span.textContent)
-                                            if(span.style.fontWeight === 'bold') textContent = `**${textContent}**`
-                                            if(span.style.fontStyle === 'italic') textContent = `*${textContent}*`
-                                        }
-                                    }
-                                    content.push(textContent)
-                                }
-                            }
+                let image
+                let contents = []
+                for(let line of lastNews.contents.split('\n')) {
+                    if(line.match(/\[\/?list\]/)) continue
+                    if(line.match(/(STEAM_CLAN_IMAGE|STEAM_CLAN_LOC_IMAGE)/)) {
+                        if(!image) {
+                            image = line.replace(/\[img\]\{STEAM_CLAN_IMAGE\}(.+)\[\/img\]/, `${STEAM_CLAN_IMAGE}$1`)
+                            image = image.replace(/\[img\]\{STEAM_CLAN_LOC_IMAGE\}(.+)\[\/img\]/, `${STEAM_CLAN_LOC_IMAGE}$1`)
                         }
-                        infos.content = content.join('\n')
+                        continue
                     }
-                    return infos
-                })
-            }
-
-            await browser.close()
-
-            if(updateInfos.title) {
-                const newUpdate = await BSUpdates.findOne({
-                    where: {
-                        title: updateInfos.title
-                    }
-                })
-
-                if(updateInfos.image) {
-                    try {
-                        const imageRequest = await fetch(updateInfos.image)
-                        if(!imageRequest.ok) throw new Error('Récupération de l\'image impossible')
-                        const image = await imageRequest.arrayBuffer()
-                        updateInfos.image = Buffer.from(image)
-                    } catch(err) {
-                        updateInfos.image = null
-                    }
+                    line = line.replace(/\[previewyoutube=([a-zA-Z0-9-_]+)(?:;[a-z]+)?\]\[\/previewyoutube\]/g, 'https://youtu.be/$1')
+                    line = line.replace(/\[b\](.+)\[\/b\]/g, '**$1**')
+                    line = line.replace(/\[i\](.+)\[\/i\]/g, '*$1*')
+                    line = line.replace(/\[u\](.+)\[\/u\]/g, '__$1__')
+                    line = line.replace(/\[spoiler\](.+)\[\/spoiler\]/g, '||$1||')
+                    line = line.replace(/\[url=(.+)\](.+)\[\/url\]/g, '$2: $1')
+                    line = line.replace(/\[\*\]/g, '•')
+                    contents.push(line)
                 }
 
-                if(!newUpdate) {
-                    Logger.log('BSUpdate', 'INFO', 'Nouvelle mise à jour de Beat Saber')
-                    await BSUpdates.create(updateInfos)
-                    return updateInfos
+                updateData.image = image
+                updateData.content = contents.join('\n')
+            }
+        }
+
+        if(updateData.title) {
+            const newUpdate = await BSUpdates.findOne({
+                where: {
+                    title: updateData.title
+                }
+            })
+
+            if(updateData.image) {
+                try {
+                    const imageRequest = await fetch(updateData.image)
+                    if(!imageRequest.ok) throw new Error('Récupération de l\'image impossible')
+                    const image = await imageRequest.arrayBuffer()
+                    updateData.image = Buffer.from(image)
+                } catch(err) {
+                    updateData.image = null
                 }
             }
 
-            return null
-        } catch(error) {
-            await browser.close()
-            console.log(error)
-            Logger.log('BSUpdate', 'ERROR', 'Une erreur est survenue lors de la récupération des mises à jour')
-            return null
+            if(!newUpdate) {
+                Logger.log('BSUpdate', 'INFO', 'Nouvelle mise à jour de Beat Saber')
+                await BSUpdates.create(updateData)
+                return updateData
+            }
         }
     },
 
@@ -136,6 +97,7 @@ export default {
      */
     async postUpdate(client, update) {
         const guild = client.guilds.cache.get(config.guild.id)
+        /** @type {TextChannel} */
         const updateChannel = guild.channels.cache.get(config.guild.channels.bsUpdates)
 
         if(update.title && update.content) await updateChannel.send({ content: roleMention(config.guild.roles['Beat Saber Update']) })
@@ -148,7 +110,7 @@ export default {
             for(let i = 0; i < content.length; i++) {
                 message.push(content[i])
                 if(message.join('\n').length + content[i].length >= 1500 || i === content.length - 1) {
-                    await updateChannel.send({ content: message.join('\n') })
+                    await updateChannel.send({ content: message.join('\n'), flags: MessageFlags.SuppressEmbeds })
                     message = []
                 }
             }
