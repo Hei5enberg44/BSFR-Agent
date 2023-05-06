9ae48c56edc00f887601838d637bc81182fbf251
Hei5enberg44 <antoineboulet44@gmail.com>
2023-05-06 18:25:03 +0200

    Ajout de la commande "/rejoindre"

---
+++ commands/join.js
@@ -0,0 +1,56 @@
+import { SlashCommandBuilder, PermissionFlagsBits, ChannelType, VoiceChannel, CommandInteraction, userMention, channelMention } from 'discord.js'
+import { joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice'
+import { CommandError } from '../utils/error.js'
+import Locales from '../utils/locales.js'
+import Logger from '../utils/logger.js'
+import config from '../config.json' assert { type: 'json' }
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('join')
+        .setNameLocalization('fr', 'rejoindre')
+        .setDescription('Join a voice channel')
+        .setDescriptionLocalization('fr', 'Rejoindre à salon vocal')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setNameLocalization('fr', 'salon')
+                .setDescription('Channel to join')
+                .setDescriptionLocalization('fr', 'Salon à rejoindre')
+                .addChannelTypes(ChannelType.GuildVoice)
+                .setRequired(true)
+        )
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
+    ,
+
+    /**
+     * Exécution de la commande
+     * @param {CommandInteraction} interaction interaction Discord
+     */
+    async execute(interaction) {
+        try {
+            /** @type {VoiceChannel} */
+            const channel = interaction.options.getChannel('channel')
+
+            const voiceConnection = joinVoiceChannel({
+                guildId: config.guild.id,
+                channelId: channel.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator
+            })
+
+            voiceConnection.on(VoiceConnectionStatus.Destroyed, () => {
+                Logger.log('JoinCommand', 'INFO', `@${interaction.client.user.username} a quitté 🔊${channel.name}`)
+            })
+
+            Logger.log('JoinCommand', 'INFO', `@${interaction.client.user.username} a rejoint 🔊${channel.name}`)
+
+            await interaction.reply({ content: Locales.get(interaction.locale, 'voice_channel_joined', userMention(interaction.client.user.id), channelMention(channel.id)), ephemeral: true })
+        } catch(error) {
+            if(error.name === 'COMMAND_INTERACTION_ERROR' || error.name === 'QUOTA_LIMIT_ERROR') {
+                throw new CommandError(error.message, interaction.commandName)
+            } else {
+                throw Error(error.message)
+            }
+        }
+    }
+}
