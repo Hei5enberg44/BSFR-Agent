aae01dd968d1e826b782e67a66bf8e8c96f24e9a
Hei5enberg44 <antoineboulet44@gmail.com>
2023-05-08 03:08:04 +0200

    Optimisation du système de chargement des commandes

--- controllers/commands.js
+++ controllers/commands.js
@@ -1,5 +1,5 @@
 import * as fs from 'node:fs'
-import { Client, Collection, EmbedBuilder, InteractionType, ApplicationCommandOptionType, channelMention } from 'discord.js'
+import { Client, Collection, EmbedBuilder, BaseInteraction, ApplicationCommandOptionType, channelMention } from 'discord.js'
 import { CommandError } from '../utils/error.js'
 import Locales from '../utils/locales.js'
 import Logger from '../utils/logger.js'
@@ -62,46 +62,52 @@ export default class Commands {
      * Écoute des saisies de commandes
      */
     async listen() {
-        this.client.on('interactionCreate', async interaction => {
-            if(interaction.type !== InteractionType.ApplicationCommand) return
-        
-            const command = this.client.commands.get(interaction.commandName)
-        
-            if(!command) return
-        
-            try {
-                const commandOptions = this.getCommandOptions(interaction.options.data)
-                Logger.log('CommandManager', 'INFO', `${interaction.user.tag} a exécuté la commande "/${interaction.commandName}${commandOptions.length > 0 ? ` ${commandOptions.join(' ')}` : ''}"`)
+        this.client.on('interactionCreate',
+            /**
+             * @param {BaseInteraction} interaction The interaction which was created
+             */
+            async (interaction) => {
+                if(!interaction.isChatInputCommand()) return
 
-                // On test si la commande est exécutée depuis le bon channel
-                if(command.allowedChannels) {
-                    for(const channel of command.allowedChannels) {
-                        if(channel !== interaction.channelId) {
-                            throw new CommandError(Locales.get(interaction.locale, 'wrong_channel', command.allowedChannels.map(channel => channelMention(channel)).join('\n')), interaction.commandName)
+                /** @type {{data: object, ?allowedChannels: Array<string>, execute: function}} */
+                const command = this.client.commands.get(interaction.commandName)
+
+                if(!command) return
+
+                try {
+                    const commandOptions = this.getCommandOptions(interaction.options.data)
+                    Logger.log('CommandManager', 'INFO', `${interaction.user.tag} a exécuté la commande "/${interaction.commandName}${commandOptions.length > 0 ? ` ${commandOptions.join(' ')}` : ''}"`)
+
+                    // On test si la commande est exécutée depuis le bon channel
+                    if(command.allowedChannels) {
+                        for(const channel of command.allowedChannels) {
+                            if(channel !== interaction.channelId) {
+                                throw new CommandError(Locales.get(interaction.locale, 'wrong_channel', command.allowedChannels.map(channel => channelMention(channel)).join('\n')), interaction.commandName)
+                            }
                         }
                     }
-                }
 
-                await command.execute(interaction)
-            } catch(error) {
-                let errMessage
-                if(error.name === 'COMMAND_ERROR') {
-                    errMessage = error.message
-                } else {
-                    errMessage = Locales.get(interaction.locale, 'command_error')
-                    Logger.log('CommandManager', 'ERROR', `L'exécution de la commande "/${interaction.commandName}" a échoué : ${error.message}`)
-                }
+                    await command.execute(interaction)
+                } catch(error) {
+                    let errMessage
+                    if(error.name === 'COMMAND_ERROR') {
+                        errMessage = error.message
+                    } else {
+                        errMessage = Locales.get(interaction.locale, 'command_error')
+                        Logger.log('CommandManager', 'ERROR', `L'exécution de la commande "/${interaction.commandName}" a échoué : ${error.message}`)
+                    }
 
-                const embed = new EmbedBuilder()
-                        .setColor('#E74C3C')
-                        .setDescription(`❌ ${errMessage}`)
-                
-                if(!interaction.deferred && !interaction.replied) {
-                    await interaction.reply({ embeds: [embed], ephemeral: true })
-                } else {
-                    await interaction.editReply({ embeds: [embed] })
+                    const embed = new EmbedBuilder()
+                            .setColor('#E74C3C')
+                            .setDescription(`❌ ${errMessage}`)
+
+                    if(!interaction.deferred && !interaction.replied) {
+                        await interaction.reply({ embeds: [embed], ephemeral: true })
+                    } else {
+                        await interaction.editReply({ embeds: [embed] })
+                    }
                 }
             }
-        })
+        )
     }
 }
